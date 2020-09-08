c22586fa25b855ec8be381a4b17f072b134b4c87
Janchooon <janson1999@gmail.com>
2020-09-08 18:37:47 -0300

    Update RailFence.py

--- Delgado-Gomez_Lab/RailFence.py
+++ Delgado-Gomez_Lab/RailFence.py
@@ -1,9 +1,9 @@
 # Función que encripta el mensaje txt
 def Encriptar(Texto, Niveles): 
 
-	# Crea una matriz a partir de la palabra a cifrar y lo niveles de encriptación
-	Matriz = [['\n' for i in range(len(Texto))] 
-				for j in range(Niveles)] 
+	# Crea una matriz a partir de la palabra a cifrar y los niveles de encriptación
+	Matriz = [['\n' for i in range(len(Texto))]  #Eje X
+				for j in range(Niveles)]  #Eje Y
 	
 	# Se define un punto de inicio ligado a la matriz, en donde se empieza por el 0,0
 	Bajar = False
@@ -12,48 +12,37 @@ def Encriptar(Texto, Niveles):
 	
 	for i in range(len(Texto)): 
 		
-		# check the direction of flow 
-		# reverse the direction if we've just 
-		# filled the top or bottom Matriz 
+		#No baja la letra si es la primera
 		if (Fila == 0) or (Fila == Niveles - 1): 
 			Bajar = not Bajar 
 		
-		# fill the corresponding alphabet 
-		Matriz[Fila][Columna] = Texto[i] 
+		Matriz[Fila][Columna] = Texto[i] #Que escriba en la posición de la matriz la letra que se encuentra en ese instante
 		Columna += 1
 		
-		# find the next Fila using 
-		# direction flag 
+		#Al agregar una letra que aumente la posición de la fila
 		if Bajar: 
 			Fila += 1
 		else: 
 			Fila -= 1
-	# now we can construct the Texto_Cifrado 
-	# using the Matriz matrix 
-	Resultado = [] 
+	 
+	Resultado = [] #Lista en donde se alojan las letras en el orden de encriptación según el nivel del rail fence 
 	for i in range(Niveles): 
 		for j in range(len(Texto)): 
 			if Matriz[i][j] != '\n': 
 				Resultado.append(Matriz[i][j]) 
-	return("" . join(Resultado)) 
+	return("" . join(Resultado)) #Intersecta las letras correspondientes a la lista resultado
 	
-# This function receives Texto_Cifrado-Texto 
-# and Niveles and returns the original 
-# Texto after decryption 
+#Esta función se encarga de desencriptar el txt
 def Desencriptar(Texto_Cifrado, Niveles): 
 
-	# create the matrix to Texto_Cifrado 
-	# plain Texto Niveles = Filas , 
-	# length(Texto) = Columnaumnas 
-	# filling the Matriz matrix to 
-	# distinguish filled spaces 
-	# from blank ones 
-	Matriz = [['\n' for i in range(len(Texto_Cifrado))] 
-				for j in range(Niveles)] 
+	# Crea una matriz a partir de la palabra a cifrar y los niveles de encriptación
+	Matriz = [['\n' for i in range(len(Texto))]  #Eje X
+				for j in range(Niveles)]  #Eje Y
 	
-	# to find the direction 
-	Bajar = None
-	Fila, Columna = 0, 0
+	# Se define un punto de inicio ligado a la matriz, en donde se empieza por el 0,0
+	Bajar = False
+	Fila = 0
+	Columna = 0
 	
 	# mark the places with '*' 
 	for i in range(len(Texto_Cifrado)): 
