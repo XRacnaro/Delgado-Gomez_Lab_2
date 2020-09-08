ca175cb0aafc85239d80549d33f67aa6de5d0281
Janchooon <janson1999@gmail.com>
2020-09-08 18:45:24 -0300

    Update Cesar.py

--- Delgado-Gomez_Lab/Cesar.py
+++ Delgado-Gomez_Lab/Cesar.py
@@ -1,6 +1,8 @@
+
+#Se importam las letras mayusculas y minusculas ligadas al código ascci
 from string import ascii_lowercase as lc
 from string import ascii_uppercase as uc
 
 def Rot(n):
-    Buscar = str.maketrans(lc + uc, lc[n:] + lc[:n] + uc[n:] + uc[:n])
-    return lambda s: s.translate(Buscar)
+    Buscar = str.maketrans(lc + uc, lc[n:] + lc[:n] + uc[n:] + uc[:n]) #Toma las letras pertenecientes a la oración o texto y las reposiciona por medio de operaciones
+    return lambda s: s.translate(Buscar) #Pasa la palabra u oración de ascci a texto
