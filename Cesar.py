f8c1ba44eaeabe37148b3f2151245b551deb1597
Janchooon <janson1999@gmail.com>
2020-09-08 16:55:57 -0300

    Delete Cesar.py

--- Cesar.py
+++
@@ -1,6 +0,0 @@
-from string import ascii_lowercase as lc
-from string import ascii_uppercase as uc
-
-def Rot(n):
-    Buscar = str.maketrans(lc + uc, lc[n:] + lc[:n] + uc[n:] + uc[:n])
-    return lambda s: s.translate(Buscar)
